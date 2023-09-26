412c52de22591709cb27f430c6b2d7c9c455cce6
Pablo Piñas <osoyinas@gmail.com>
2023-09-26 16:02:04 +0200

    Improve admin site

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from tickets.models import Ticket
 # Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name','paid','price', 'number', 'email', 'date']
+    search_fields = ['name', 'email'] 
