46f51e48b4b28ccc78b5be124bcfb3a0b4c30f0a
Pablo Piñas <osoyinas@gmail.com>
2023-09-26 15:53:20 +0200

    Add atributos to ticker

--- tickets/models.py
+++ tickets/models.py
@@ -5,6 +5,9 @@ class Ticket(models.Model):
     number = models.CharField(max_length=15)
     email = models.EmailField()
     paid = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    companions = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.name)
