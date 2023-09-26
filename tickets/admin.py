8e483dd3779b1d4b8337e24a423410cb511e379b
Pablo Piñas <osoyinas@gmail.com>
2023-09-26 16:14:10 +0200

    Improve admin site

--- tickets/admin.py
+++ tickets/admin.py
@@ -4,5 +4,12 @@ from tickets.models import Ticket
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name','paid','price', 'number', 'email', 'date']
+    list_display = ['id', 'name','paid','formatted_price', 'number', 'email', 'date']
+    def formatted_price(self, obj):
+        # Formatea el precio con un símbolo de dólar ($) y dos decimales
+        return f"{obj.price:.2f}Є"
+    formatted_price.short_description = 'Precio'  # Define un encabezado personalizado para la columna
     search_fields = ['name', 'email'] 
+    list_editable = ['paid']
+    list_filter = ['paid']
+    sortable_by = ['price']
