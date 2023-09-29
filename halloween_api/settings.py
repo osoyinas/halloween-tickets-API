45cac3f08e1c24763cd6f56ce48993e9e1d97862
Pablo Piñas <osoyinas@gmail.com>
2023-09-29 11:02:30 +0200

    Deactive csrf with variables

--- halloween_api/settings.py
+++ halloween_api/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "django.middleware.csrf.CsrfViewMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -152,6 +153,8 @@ CORS_ALLOWED_ORIGINS = [
 
 HOST_URL = f"https://{os.environ.get('RAILWAY_PUBLIC_DOMAIN')}"
 
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_HTTPONLY = False
 
 CSRF_TRUSTED_ORIGINS=[HOST_URL, "https://halloween-13.netlify.app"]
 CORS_ALLOW_CREDENTIALS = True
