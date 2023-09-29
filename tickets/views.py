717cfbf4315435e26927c33fa177f6bf74ad76cf
Pablo Piñas <osoyinas@gmail.com>
2023-09-29 11:10:08 +0200

    allow any and add custom middleware

--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .models import Ticket, Person
 from .serializers import TicketSerializer
 
@@ -8,5 +9,6 @@ from .serializers import TicketSerializer
 class CreateTicketAPIView(generics.CreateAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [AllowAny]
 
 CreateTicketFinalView = csrf_exempt(CreateTicketAPIView.as_view())
