1ea3d9744f1a0b0e009baedb10f690f717a65bfc
Pablo Piñas <osoyinas@gmail.com>
2023-09-29 10:41:48 +0200

    Add netlify to CSRF trusted

--- halloween_api/settings.py
+++ halloween_api/settings.py
@@ -154,4 +154,4 @@ CORS_ALLOWED_ORIGINS = [
 HOST_URL = f"https://{os.environ.get('RAILWAY_PUBLIC_DOMAIN')}"
 
 
-CSRF_TRUSTED_ORIGINS=[HOST_URL]
+CSRF_TRUSTED_ORIGINS=[HOST_URL, "https://halloween-13.netlify.app"]
