640af2948d2b4702968c8f556708c082c29b8a0d
Pablo Piñas <osoyinas@gmail.com>
2023-10-01 13:57:04 +0200

    Change qr url

--- tickets/email.py
+++ tickets/email.py
@@ -29,7 +29,8 @@ def notify_ticket_to_admin(ticket):
 QR_API = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='
 
 def send_ticket_to_titular(ticket):
-    context = {'titular':ticket.titular,'qr': f"{QR_API}{quote(settings.HOST_URL, safe='')}", 'id':ticket.id }
+    url_check = settings.HOST_URL + 'admin/tickets/ticket/'+ ticket.id
+    context = {'titular':ticket.titular,'qr': f"{QR_API}{quote(url_check, safe='')}", 'id':ticket.id }
     html_content = render_to_string("email_template.html",context)
     text_content = strip_tags(html_content)
     email = EmailMultiAlternatives(
