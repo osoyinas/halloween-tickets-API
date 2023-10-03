53ff5026ad4108345577fcd15a91683528e67f2b
Pablo Piñas <osoyinas@gmail.com>
2023-10-03 16:45:11 +0200

    Change email to .env

--- halloween_api/settings.py
+++ halloween_api/settings.py
@@ -159,8 +159,8 @@ HOST_URL = os.environ.get('HOST_URL')
 # EMAIL CONFIG
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'osoyinas@gmail.com'
-EMAIL_HOST_PASSWORD = 'plkcohcplylfieam'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
